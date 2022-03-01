0c2369250ac5b14ff9cb4a521b2ef968749ab941
timfeirg <kkcocogogo@gmail.com>
2022-03-01 13:51:10 +0800

    [lain prepare] fix keep bug
    closes https://github.com/timfeirg/lain-cli/issues/16

--- tests/test_workflow.py
+++ tests/test_workflow.py
@@ -62,12 +62,15 @@ def test_build(registry):
             'prepare_env': BUILD_TREASURE_NAME,
             'escape_test': 'space test & newline \n test',
         }
-        build_clause['prepare']['keep'].append('another/thing.txt')
+        build_clause['prepare']['keep'].extend([
+            'foo/thing.txt',
+            'bar',
+        ])
         build_clause['prepare']['script'].extend(
             [
                 f'echo {RANDOM_STRING} > {BUILD_TREASURE_NAME}',
-                'mkdir another',
-                f'echo {RANDOM_STRING} > another/thing.txt',
+                'mkdir foo bar',
+                'touch foo/thing.txt bar/thing.txt',
             ]
         )
         lain_build(stage=stage)
@@ -77,7 +80,7 @@ def test_build(registry):
     res = docker_run(prepare_image, ['ls'])
     ls_result = parse_ls(res.stdout)
     # ensure keep clause works as expected
-    assert ls_result == {'another', BUILD_TREASURE_NAME}
+    assert ls_result == {'foo', 'bar', BUILD_TREASURE_NAME}
     res = docker_run(prepare_image, ['env'])
     envs = ensure_str(res.stdout).splitlines()
     assert f'prepare_env={BUILD_TREASURE_NAME}' in envs
